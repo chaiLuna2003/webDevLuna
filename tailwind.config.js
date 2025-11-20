2e147073c743bb3f83ff6f23afd71398ab531026
AngelLuna <231241045@alumnos.utn.edu.mx>
2025-11-20 17:54:31 -0600

    Actualizacion del index

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        "mi-color-personalizado": "#123456",
+        "mi-otro-color": {
+          100: "#abcdef",
+          200: "#123abc",
+        },
+      },
+    },
+  },
+}
